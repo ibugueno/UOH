839e447c44f184cfdbb8b567294262531316ce84
ibugueno <ignacio.bugueno@live.com>
2020-09-29 13:37:29 -0300

    using gpu #0

--- dgx1-gan/run_docker.sh
+++ dgx1-gan/run_docker.sh
@@ -2,6 +2,6 @@
 
 docker rm -fv ignacio_gan
 
-nvidia-docker run -it -e NVIDIA_VISIBLE_DEVICES=3 --name ignacio_gan -v /home/ignacio.bugueno/cachefs:/app/data ignacio_gan
+nvidia-docker run -it -e NVIDIA_VISIBLE_DEVICES=0 --name ignacio_gan -v /home/ignacio.bugueno/cachefs:/app/data ignacio_gan
 
 sleep 10; docker logs ignacio_gan
